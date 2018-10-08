77e731b3ec60cd91db821fdf0c2cb1f848b07dd8
bobbyren <bobbyren@gmail.com>
2018-10-07 21:21:53 -0400

    added PostViewController and PostService

---
+++ RedditClone/Services/PostService.swift
@@ -0,0 +1,13 @@
+//
+//  PostService.swift
+//  RedditClone
+//
+//  Created by Bobby Ren on 10/7/18.
+//  Copyright © 2018 RenderApps LLC. All rights reserved.
+//
+
+import UIKit
+
+class PostService: NSObject {
+
+}
